6d75ac174882ed3d71399bb0cc85c5b710492cb0
zhouzhenhua <275291220@qq.com>
2024-04-08 22:47:07 +0800

    Update and rename test.py to index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import logging
+import json
+
+# To enable the initializer feature (https://help.aliyun.com/document_detail/158208.html)
+# please implement the initializer function as below：
+# def initializer(context):
+#   logger = logging.getLogger()
+#   logger.info('initializing')
+
+def handler(event, context):
+  # evt = json.loads(event)
+  logger = logging.getLogger()
+  logger.info('hello world i love u')
+  return 'hello world i love u'
